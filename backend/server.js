5753e0c63886ad4b3dec552ff1d77d24022cf5c0
sreelakshmi20056 <trsreelakshmi92@gmail.com>
2026-03-24 13:09:54 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 const authRoutes = require("./routes/authRoutes");
 const classRoutes = require("./routes/classRoutes");
